340b2c1b7b2be2193c176b7edff64a8414da5c0b
madhuri8211 <120010363+madhuri8211@users.noreply.github.com>
2022-12-08 15:57:52 +1300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let numbersArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
+    cubeWrapper = document.getElementById('cube-container'), appendCubeElement;
+
+numbersArray.forEach((item, index) => {
+    cubeWrapper.innerHTML += '<li data-item=' + item + '>' + item + '</li>'
+})
+
+shuffleNumbers = () => {
+    let index = 0
+    numbersArray = numbersArray.sort(() => { return 0.5 - Math.random() });
+    cubeWrapper.innerHTML = ''
+    while (index < numbersArray.length) {
+        let cubeItem = document.createElement("li");
+        cubeItem.innerHTML = numbersArray[index]
+        cubeItem.setAttribute('data-item', numbersArray[index])
+        cubeWrapper.appendChild(cubeItem)
+        ++index;
+    }
+}
+sortNumbers = () => {
+    let index = 0
+    numbersArray = numbersArray.sort((a, b) => { return a - b });
+    cubeWrapper.innerHTML = ''
+    while (index < numbersArray.length) {
+        let cubeItem = document.createElement("li");
+        cubeItem.innerHTML = numbersArray[index]
+        cubeItem.setAttribute('data-item', numbersArray[index])
+        cubeWrapper.appendChild(cubeItem)
+        ++index;
+    }
+};
